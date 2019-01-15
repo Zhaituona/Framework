95521453baca37e9f85c71e96cbf517d983ab904
Zhaituona <zaytuna128@gmail.com>
2019-01-15 11:07:25 -0500

    added

--- Generic/src/main/java/base/CommonClass.java
+++ Generic/src/main/java/base/CommonClass.java
@@ -3,6 +3,7 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,6 +15,9 @@ public class CommonClass  {
 
     public static WebDriver driver;
     public static Properties prop;
+    public  static EventFiringWebDriver e_driver;
+    public static WebEventListener eventListener;
+
 
     public CommonClass() {
         try {
@@ -36,6 +40,14 @@ public class CommonClass  {
             System.setProperty("webdriver.gecko.driver", "C:\\Users\\Elzat\\IdeaProjects\\Auto3\\Generic\\browser-driver\\geckodriver.exe");
             driver = new FirefoxDriver();
         }
+
+
+        e_driver = new EventFiringWebDriver(driver);
+        // Now create object of EventListerHandler to register it with EventFiringWebDriver
+        eventListener = new WebEventListener();
+        e_driver.register(eventListener);
+        driver = e_driver;
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(Util.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
